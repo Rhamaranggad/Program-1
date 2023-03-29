a188ad9c86766c5f65bfa2bd7d031c8d499a87d0
Rhamaranggad <127860723+Rhamaranggad@users.noreply.github.com>
2023-03-29 17:11:26 +0700

    Add files via upload

---
+++ Taxi/Taxi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramTaxi
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Taxi Taxi = new Taxi();
+
+            Taxi.Name = "Usep";
+            Taxi.Duty = true;
+            Taxi.Passanger = 5;
+
+            Taxi.TaxiInfo();
+            Taxi.PickUpPassanger();
+            Taxi.DropOffPassanger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi/Taxi/Taxi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramTaxi
+{
+    public class Taxi
+    {
+        public string Name { get; set; }
+        public bool Duty { get; set; }
+        public int Passanger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver : {0}", Name );
+            Console.WriteLine( "Duty: {0}", Duty);
+            Console.WriteLine("Number of Passanger: {0}", Passanger);
+        }
+        public void PickUpPassanger()
+        {
+            Console.WriteLine("\nUsep sedang Menjemput Penumpang");
+        }
+        public void DropOffPassanger()
+        {
+            Console.WriteLine("\nUsep Selesai Mengantar Penumpang");
+        }
+    }
+}
